e6faa315d2cdd857aa12a8f30529a7907874a824
ayeshasiddiqua29 <117896029+ayeshasiddiqua29@users.noreply.github.com>
2022-11-10 13:17:00 +0530

    Add files via upload

---
+++ web design internship.py
@@ -0,0 +1,77 @@
+<br>
+<head style="background-color:rgb(228, 115, 115);">
+<title>Lab Program 3</title>
+<script language="JavaScript">
+function welcome() {
+alert("Welcome to international Airport");
+}
+function kingfisher() {
+alert("Welcome to Kingfisher");
+}
+function IndianAirlines() {
+alert("Welcome to Indian Airlines");
+}
+function JetAirways() {
+alert("Welcome to Jet Airways");
+}
+</script>
+</head>
+<br onLoad="return welcome();">
+<center>
+<h1 style="font-style: italic; font-size: 50px; color: rgb(51, 186, 204) ;background-color: tan;">
+WELOCME TO INTERNATIONAL AIRPORT
+</h1>
+</center>
+<center>
+<h3 style="color:teal;font-size:30px; text-align:center;">
+<input
+type="radio"
+name="b1"
+value="click here"
+onclick="return kingfisher();"
+/>
+<em>KINGFISHER</em>
+</h3>
+</br>
+<img style="border: 10px outsideS cornflowerblue " src="C:\Users/Ayesha%20Siddiqua/Downloads/kingfisher-a330-200-vt-vjk-05grd-lhr-a6lr.webp"
+height="100" width="200" class="center" />
+<br />
+<p style="font-size: 20px; text-align: center;" >Want to book a ticket just click below</p>
+<a style="text-align: center;" href="https://www.makemytrip.com/flights/"> book a ticket &rarr;</a>
+</center>
+<center>
+<h3 style="color:teal;font-size: 30px;text-align: center;">
+<input
+type="radio"
+name="b1"
+value="click here"
+onclick="return IndianAirlines();"
+/>
+<em> INDIAN AIRLINES</em>
+</h3>
+<br />
+<img style="border:10px outside cornflowerblue" src="C:\Users\Ayesha Siddiqua\Downloads\vt-esd-indian-airlines-airbus-a320-231_PlanespottersNet_324245.jpg"
+height="100" width="200" class="center"/>
+</br>
+<p style="font-size: 20px; text-align: center;">Want to book a ticket just click below</p>
+<a style="text-align: center;" href="https://www.makemytrip.com/flights/"> book a ticket &rarr;</a>
+</center>
+<center>
+<h3 style="color:teal;font-size: 30px;text-align: center;">
+<input
+type="radio"
+name="b1"
+value="click here"
+onclick="return emirates();"
+/>
+<em> EMIRATES</em>
+</h3>
+</br>
+<img style="border:10px outside cornflowerblue" src="C:\Users\Ayesha Siddiqua\Downloads\EK_777_KPAE_katie_bailey_2-scaled-1.jpeg"
+height="100" width="200" class="center"/>
+</br>
+<p style="font-size: 20px; text-align: center;">Want to book a tickrt just click below</p>
+<a style="text-align: center;" href="https://www.makemytrip.com/flights/">book a ticket &rarr;</a>
+</center>
+</body>
+</html>
